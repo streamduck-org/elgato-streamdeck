cacfb0497e6db16ac9ca2bd0573edd94e5b5767a
TheJebForge <github@thejebforge.com>
2024-05-16 23:49:47 +0300

    Updated examples to use clear all methods and handle other events

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -19,14 +19,14 @@ async fn main() {
                 println!("Connected to '{}' with version '{}'", device.serial_number().unwrap(), device.firmware_version().unwrap());
 
                 device.set_brightness(50).unwrap();
-                device.clear_button_image(0xff).unwrap();
+                device.clear_all_button_images().unwrap();
                 // Use image-rs to load an image
                 let image = open("no-place-like-localhost.jpg").unwrap();
 
                 // device.set_logo_image(image.clone()).unwrap();
 
                 device.set_brightness(50).unwrap();
-                device.clear_button_image(0xff).unwrap();
+                device.clear_all_button_images().unwrap();
 
                 println!("Key count: {}", kind.key_count());
                 // Write it to the device
@@ -43,29 +43,43 @@ async fn main() {
                 {
                     let reader = device.get_reader();
 
-                    let updates = match reader.read(Some(Duration::from_secs_f64(100.0))) {
-                        Ok(updates) => updates,
-                        Err(_) => break,
-                    };
-                    for update in updates {
-                        match update {
-                            DeviceStateUpdate::ButtonDown(key) => {
-                                println!("Button {} down", key);
-                            }
-                            DeviceStateUpdate::ButtonUp(key) => {
-                                println!("Button {} up", key);
-                            }
-                            DeviceStateUpdate::EncoderTwist(dial, ticks) => {
-                                println!("Dial {} twisted by {}", dial, ticks);
-                            }
-                            DeviceStateUpdate::EncoderDown(dial) => {
-                                println!("Dial {} down", dial);
-                            }
-                            DeviceStateUpdate::EncoderUp(dial) => {
-                                println!("Dial {} up", dial);
-                            }
-                            _ => {
-                                println!("Unknown update");
+                    'infinite: loop {
+                        let updates = match reader.read(Some(Duration::from_secs_f64(100.0))) {
+                            Ok(updates) => updates,
+                            Err(_) => break,
+                        };
+                        for update in updates {
+                            match update {
+                                DeviceStateUpdate::ButtonDown(key) => {
+                                    println!("Button {} down", key);
+                                }
+                                DeviceStateUpdate::ButtonUp(key) => {
+                                    println!("Button {} up", key);
+                                    if key == device.kind().key_count() - 1 {
+                                        break 'infinite;
+                                    }
+                                }
+                                DeviceStateUpdate::EncoderTwist(dial, ticks) => {
+                                    println!("Dial {} twisted by {}", dial, ticks);
+                                }
+                                DeviceStateUpdate::EncoderDown(dial) => {
+                                    println!("Dial {} down", dial);
+                                }
+                                DeviceStateUpdate::EncoderUp(dial) => {
+                                    println!("Dial {} up", dial);
+                                }
+
+                                DeviceStateUpdate::TouchScreenPress(x, y) => {
+                                    println!("Touch Screen press at {x}, {y}")
+                                }
+                                
+                                DeviceStateUpdate::TouchScreenLongPress(x, y) => {
+                                    println!("Touch Screen long press at {x}, {y}")
+                                }
+                                
+                                DeviceStateUpdate::TouchScreenSwipe((sx, sy), (ex, ey)) => {
+                                    println!("Touch Screen swipe from {sx}, {sy} to {ex}, {ey}")
+                                }
                             }
                         }
                     }
